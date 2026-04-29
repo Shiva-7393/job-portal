47ca0cdae9c88d74214f4f2cf6599918466b63fc
Udugundla Shiva <shivaudugundla73@gmail.com>
2026-04-29 19:00:54 +0530

    fix: update API_BASE_URL to Render backend URL

--- frontend/frontend/src/api.js
+++ frontend/frontend/src/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000/api'
+export const API_BASE_URL = 'https://job-portal-2-6ogw.onrender.com/api'
 
 const parseResponse = async (response) => {
     const data = await response.json()
